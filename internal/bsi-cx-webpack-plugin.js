363686b6c8495408670f6efdb532c392189aa1df
Oliver Fabel <oliver.fabel@students.fhnw.ch>
2021-07-05 11:23:39 +0200

    work in progress

--- internal/bsi-cx-webpack-plugin.js
+++ internal/bsi-cx-webpack-plugin.js
@@ -3,60 +3,92 @@ const fs = require('fs');
 const { createHash } = require('crypto');
 
 class BsiCxWebpackPlugin {
-  constructor(compiler) {
+  constructor(compiler, stats) {
     this._compiler = compiler;
+    this._stats = stats;
     this._outputPath = compiler.options.output.path;
-    this._entryPath = path.resolve(this._outputPath, 'design.json.js');
     this._designJsonPath = path.resolve(this._outputPath, 'design.json');
-    this._designHtmlPath = path.resolve(this._outputPath, 'design.html');
-    this._previewHtmlPath = path.resolve(this._outputPath, 'preview.html');
+    this._designHtmlPath = path.resolve(this._outputPath, stats.compilation.entrypoints.get('design').options.filename.replace(/\.js$/, ''));
+    this._previewHtmlPath = path.resolve(this._outputPath, stats.compilation.entrypoints.get('preview').options.filename.replace(/\.js$/, ''));
   }
 
   async apply() {
-    let template = this._loadTemplate();
-
-    this._exportDesignJson(template);
+    this._exportDesignJson();
     this._exportDesignHtml();
-    //this._exportPreviewHtml(template.preview);
+    this._exportPreviewHtml();
   }
 
-  _loadTemplate() {
-    return require(this._entryPath);
+  /**
+   * @returns {{}}
+   */
+  _loadDesignJson() {
+    let designJsonPath = `${this._designJsonPath}.js`;
+    let designJsonObj = require(designJsonPath);
+    return designJsonObj.json;
   }
 
-  _exportDesignJson(meta) {
-    meta.designJson.contentElementGroups
+  _exportDesignJson() {
+    let designJsonObj = this._loadDesignJson();
+    designJsonObj.contentElementGroups
       .forEach(group => group.contentElements
         .forEach(element => this._handleElement(element)));
-    fs.writeFileSync(this._designJsonPath, JSON.stringify(meta, null, 4));
+    fs.writeFileSync(this._designJsonPath, JSON.stringify(designJsonObj, null, 4));
   }
 
   _handleElement(element) {
     element.file = this._handleFile(element.file);
   }
 
-  _handleFile(file) {
-    let content = file.content;
-    let extension = path.extname(file.path);
-    let filename = path.basename(file.path, extension);
+  /**
+   * @param {{content:string,path:string}} fileObj
+   * @returns {string}
+   */
+  _handleFile(fileObj) {
+    let content = fileObj.content;
+    let originalExtension = path.extname(fileObj.path);
+    let fileName = path.basename(fileObj.path, originalExtension);
     let contentHash = createHash('sha256').update(content).digest('hex').substr(0, 20);
 
-    let elementFileName = `${filename}-${contentHash}.html`;
+    let extension = this._getElementFileExtension(fileObj.path);
+    let elementFileName = `${fileName}-${contentHash}.${extension}`;
     let elementFilePath = path.resolve(this._outputPath, elementFileName);
 
-    fs.writeFileSync(elementFilePath, content);
+    fs.writeFileSync(elementFilePath, content.trim());
 
     return elementFileName;
   }
 
-  _exportDesignHtml() {
-    let template = require(`${this._designHtmlPath}.js`);
-    console.info(template.designHtml);
-    fs.writeFileSync(this._designHtmlPath, eval(template.designHtml));
+  /**
+   * @param {string} fileName
+   * @returns {string}
+   */
+  _getElementFileExtension(fileName) {
+    if (/\.hbs\.twig/.test(fileName)) {
+      return 'hbs';
+    }
+    if (/\.hbs/.test(fileName)) {
+      return 'hbs';
+    }
+    return 'html';
   }
 
-  _exportPreviewHtml(preview) {
-    fs.writeFileSync(this._previewHtmlPath, preview);
+  /**
+   * @param {string} filePath 
+   * @param {string} name 
+   */
+  _saveHtmlTemplate(filePath, name) {
+    let templatePath = `${filePath}.js`;
+    let templateObj = require(templatePath)[name];
+    let templateStr = eval(templateObj.content);
+    fs.writeFileSync(filePath, templateStr.trim());
+  }
+
+  _exportDesignHtml() {
+    this._saveHtmlTemplate(this._designHtmlPath, 'design');
+  }
+
+  _exportPreviewHtml() {
+    this._saveHtmlTemplate(this._previewHtmlPath, 'preview');
   }
 }
 
@@ -64,7 +96,7 @@ class Plugin {
   static PLUGIN_NAME = 'BsiCxWebpackPlugin';
   apply(compiler) {
     compiler.hooks.done.tapAsync(Plugin.PLUGIN_NAME, async (stats, callback) => {
-      await new BsiCxWebpackPlugin(compiler).apply();
+      await new BsiCxWebpackPlugin(compiler, stats).apply();
       callback();
     });
   }
--- internal/webpack-config-builder.js
+++ internal/webpack-config-builder.js
@@ -1,71 +1,86 @@
+const fs = require('fs');
 const path = require('path');
 
 const BsiCxWebpackPlugin = require('./bsi-cx-webpack-plugin');
 
-module.exports = (name, rootPath) => {
+const passLoader = path.resolve(__dirname, 'pass-loader.js');
+const applyLoader = path.resolve(__dirname, 'apply-loader.js');
+const templateLoader = path.resolve(__dirname, 'template-loader.js');
+
+const evaluateEntryTemplate = (rootPath, name) => {
+  let twigFilePath = path.resolve(rootPath, `${name}.twig`);
+  let hbsFilePath = path.resolve(rootPath, `${name}.hbs.twig`);
+  let isTwig = fs.existsSync(twigFilePath);
+  let extension = isTwig ? 'html' : 'hbs';
   return {
-    entry: {
-      designJson: { import: path.resolve(rootPath, 'design.js'), filename: 'design.json.js' },
-      designHtml: { import: path.resolve(rootPath, 'design.twig'), filename: 'design.html.js' }
-    },
-    name: name,
-    target: 'web',
-    module: {
-      rules: [
-        {
-          test: /\.twig$/i,
-          use: [
-            ({ realResource }) => /(design|preview)\.twig$/.test(realResource) ? path.resolve(__dirname, 'pass-loader.js') : path.resolve(__dirname, 'template-loader.js'),
-            'ref-loader',
-            ({ realResource }) => /(design|preview)\.twig$/.test(realResource) ? path.resolve(__dirname, 'pass-loader.js') : path.resolve(__dirname, 'apply-loader.js'),
-            {
-              loader: 'twing-loader',
-              options: {
-                environmentModulePath: require.resolve('./environment.js'),
-                renderContext: {}
-              }
-            }
-          ]
-        },
-        {
-          test: /\.(html|hbs)/i,
-          use: [
-            path.resolve(__dirname, 'template-loader.js'),
-            'ref-loader',
-          ]
-        },
-        {
-          test: /\.(png|jpg|jpeg)/i,
-          type: 'asset/resource',
-          generator: {
-            publicPath: '{{designBaseUrl}}/',
-            filename: 'assets/[name]-[hash][ext]'
-          }
-        },
-        {
-          resource: /static.+\.js$/i,
-          type: 'asset/resource',
-          generator: {
-            publicPath: '{{designBaseUrl}}/',
-            filename: 'assets/[name]-[hash][ext]'
-          }
-        }
-      ]
-    },
-    plugins: [
-      new BsiCxWebpackPlugin()
-    ],
-    stats: {
-      children: true
-    },
-    output: {
-      path: path.resolve(__dirname, '..', 'dist', name),
-      publicPath: '',
-      library: {
-        type: 'commonjs',
-        name: '[name]'
-      },
-      clean: true
-    }
+    import: isTwig ? twigFilePath : hbsFilePath,
+    filename: `${name}.${extension}.js`
   };
 };
+
+module.exports = (name, rootPath) => ({
+  entry: () => ({
+    json: { import: path.resolve(rootPath, 'design.js'), filename: 'design.json.js' },
+    design: evaluateEntryTemplate(rootPath, 'design'),
+    preview: evaluateEntryTemplate(rootPath, 'preview'),
+  }),
+  name: name,
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.twig$/i,
+        use: [
+          templateLoader,
+          'ref-loader',
+          ({ realResource }) => /(design|preview)\.(hbs\.twig|twig)$/.test(realResource) ? passLoader : applyLoader,
+          {
+            loader: 'twing-loader',
+            options: {
+              environmentModulePath: require.resolve('./environment.js'),
+              renderContext: {}
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(html|hbs)$/i,
+        use: [
+          templateLoader,
+          'ref-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        generator: {
+          publicPath: '{{designBaseUrl}}/',
+          filename: 'assets/[name]-[hash][ext]'
+        }
+      },
+      {
+        resource: /static.+\.js$/i,
+        type: 'asset/resource',
+        generator: {
+          publicPath: '{{designBaseUrl}}/',
+          filename: 'assets/[name]-[hash][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new BsiCxWebpackPlugin()
+  ],
+  stats: {
+    children: true
+  },
+  output: {
+    path: path.resolve(__dirname, '..', 'dist', name),
+    publicPath: '',
+    library: {
+      type: 'commonjs',
+      name: '[name]'
+    },
+    clean: true
+  }
+});
