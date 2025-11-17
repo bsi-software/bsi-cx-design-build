b5ee2b815cfcb03c7f9085c51f02b1fdc01658b5
Benni Vier <benjamin.vier@bsi-software.com>
2025-11-17 13:42:35 +0100

    Test Part

--- test/templates/website/content-elements/content/template-part-element/index.js
+++ test/templates/website/content-elements/content/template-part-element/index.js
@@ -18,9 +18,10 @@ module.exports = cx.templateElement
         cx.templatePart.Image("Bild", "image-part-01fb0560", true, ["400w", "800w", "1200w"], false),
         cx.templatePart.Checkbox("Hintergrund grün", "checkbox-part-95513657"),
         cx.templatePart.PlainText("Plain text", "plain-text-Y3dhVu"),
-        cx.templatePart.MultilinePlainText("Multiline plain text", "multiline-plain-text-0pQpp9"),
+        cx.templatePart.MultilinePlainText("Multiline plain text", "multiline-plain-text-0pQpp9", 5),
         cx.templatePart.FormattedText("Formatted text", "formatted-text-fC3iJv", require("../../../html-editor-configs/full")),
         cx.templatePart.Link("Link auf Multiline Text", "link-1wfD2H"),
         cx.templatePart.Option("Was willst du kaufen?", "option-0CxEy6", { "car": "Auto" , "house": "Haus", "yacht": "Yacht" }),
+        // Test both ways to fill option part
         // cx.templatePart.Option("Was willst du kaufen?", "option-0CxEy6", [{ "value": "car", "text": "Auto" }, { "value": "house", "text": "Haus" }, { "value": "yacht", "text": "Yacht" }])
     )
