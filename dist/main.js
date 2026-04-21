5a8add9991ac38a31be423d7b3636ad8f630dc0a
Lukas Heyer <lukas.heyer@bsi-software.com>
2026-04-21 08:58:15 +0200

    ndy

--- dist/main.js
+++ dist/main.js
@@ -2656,7 +2656,7 @@ const external_webpack_namespaceObject = require("webpack");
 ;// ./src/handlebars-helpers.js
 /* harmony default export */ const handlebars_helpers = ({
   'bsi.nls': key => key,
-  // TODO: remove if not needed
+  // TODO: remove if not needed -> seems like not necessary
   // 'bsi.hbsAttrScope': contextScope => contextScope ? "data-bsi-context-scope=\"" + contextScope + "\"" : "",
   // 'bsi.hbsVar': (variableName, contextScope) => "{{" + (contextScope ? contextScope + "." : "") + variableName + "}}"
 });
